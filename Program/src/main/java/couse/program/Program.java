d65e08ade579cf42f0934fe04e0b8164c3881637
Rodrigo-Felipe <rodrigosilva@tai.inatel.br>
2023-03-01 14:22:17 -0300

    metodos

---
+++ Program/src/main/java/couse/program/Program.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package couse.program;
+
+/**
+ *
+ * @author rodrigofelipe
+ */
+public class Program {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
