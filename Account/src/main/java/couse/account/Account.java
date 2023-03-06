4b5ec3ba5c088dd062cf88db60661aec47a67ca7
Rodrigo-Felipe <rodrigosilva@tai.inatel.br>
2023-03-06 08:19:50 -0300

    Account" git commit -m Account git commit -m 'Account

---
+++ Account/src/main/java/couse/account/Account.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package couse.account;
+
+/**
+ *
+ * @author rodrigofelipe
+ */
+public class Account {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
